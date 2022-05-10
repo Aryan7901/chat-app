f9560d825cad22641e543609fac04963fe1f2d9e
Aryan7901 <85547188+Aryan7901@users.noreply.github.com>
2022-05-11 02:06:27 +0530

    fixed issue

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,10 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 const PREFIX = "chat-app-";
-export default function useLocalStorage(key, initialValue) {
+export default function useLocalStorage(key, inValue) {
+  // eslint-disable-next-line
+  const initialValue = useMemo(() => inValue, []);
   let PREFIXED_KEY = PREFIX + key;
-  const getVal = () => {
+  const getVal = useCallback(() => {
     const jsonData = localStorage.getItem(PREFIXED_KEY);
     if (jsonData !== "null" && jsonData !== "undefined") {
       return JSON.parse(jsonData);
@@ -12,7 +14,10 @@ export default function useLocalStorage(key, initialValue) {
       localStorage.setItem(PREFIXED_KEY, JSON.stringify(initialValue));
       return initialValue;
     }
-  };
+  }, [PREFIXED_KEY, initialValue]);
+  useEffect(() => {
+    setValue(getVal());
+  }, [getVal]);
   const [value, setValue] = useState(getVal());
   useEffect(() => {
     localStorage.setItem(PREFIXED_KEY, JSON.stringify(value));
